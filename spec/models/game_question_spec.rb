dc0d90c7c8dbbdad0a36deffcf78943dada40b2e
askrynnikov <5101233@gmail.com>
2017-01-14 20:26:26 +0300

    Task 61-2
    тест на наличие методов делегатов level и text

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GameQuestion, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  let(:game_question) { FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3)}
+  let(:game_question) { FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
   context 'game status' do
     it 'correct .variants' do
@@ -16,6 +16,11 @@ RSpec.describe GameQuestion, type: :model do
 
     it 'correct .answer_correct?' do
       expect(game_question.answer_correct?('b')).to be_truthy
+    end
+
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
     end
   end
 end
