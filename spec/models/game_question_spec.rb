5601ea857c29f03e0be0f0f1f43fd42848a1056f
askrynnikov <5101233@gmail.com>
2017-01-15 08:30:25 +0300

    Task 61-5
    тесты на метод correct_answer_key модели GameQuestion.

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe GameQuestion, type: :model do
                                            })
     end
 
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq('b')
+    end
+
     it 'correct .answer_correct?' do
       expect(game_question.answer_correct?('b')).to be_truthy
     end
