3eb8c6007191a7ce313f6b0d4e91a81425221a71
askrynnikov <5101233@gmail.com>
2017-01-16 12:54:41 +0300

    Task 62-1, 62-2, 62-3

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -59,5 +59,43 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to game_path(game)
       expect(flash.empty?).to be_truthy
     end
+
+    it 'show alien game' do
+      new_user = FactoryGirl.create(:user)
+      alien_game = FactoryGirl.create(:game_with_questions, user: new_user)
+
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'user take money' do
+      game_w_questions.answer_current_question!(game_w_questions.current_game_question.correct_answer_key)
+
+      put :take_money, id: game_w_questions.id
+
+      game = assigns(:game)
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq(100)
+
+      expect(response).to redirect_to user_path(user)
+      expect(flash[:warning]).to be
+
+      expect { user.reload }.to change(user, :balance).by(100)
+    end
+
+    it 'user tries to start the second game' do
+      expect(game_w_questions.finished?).to be_falsey
+
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game)
+      expect(game).to be_nil
+
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
   end
 end
